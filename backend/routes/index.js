4ee701aa3a7748e17d4bea7e66149175514a1aae
chiku0989 <neelv448@gmail.com>
2025-01-02 17:09:48 +0530

    deleted everything
    clear

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const redditRoutes = require('./redditRoutes');
-const router = express.Router();
-
-
-router.use('/reddit', redditRoutes);
-
-module.exports = router;
