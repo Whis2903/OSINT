a03a17ce6e4d52dd084af1509d039aaa19b47376
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2025-01-02 20:43:49 +0530

    Update index.jsx

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,37 +1,37 @@
 import SearchCard from "../../components/SearchCard";
-import ResultCard from "../../components/ResultComponent";
-import { useState } from "react";
 import ResultComponent from "../../components/ResultComponent";
-const Homepage = () => {
+import { useState } from "react";
 
+const Homepage = () => {
   const [posts, setPosts] = useState([]);
 
-  const fetchData = async(keyword)=>{
+  const fetchData = async (keyword) => {
     if (!keyword.trim()) { // Double-checking that the keyword is not empty
       console.log("Keyword is required!");
       return;
     }
 
-    try{
+    try {
       const url = `http://localhost:5000/api/reddit/posts?keyword=${keyword}`;
       const response = await fetch(url);
       const data = await response.json();
-      setPosts(data)
-    } catch(e) {
-      console.error(`Error in fetching the data : ${e}` )
+      setPosts(data);
+    } catch (e) {
+      console.error(`Error in fetching the data: ${e}`);
     }
-  }
+  };
 
   return (
     <>
+      <div className="flex justify-center">
+        <SearchCard fetchData={fetchData} />
+      </div>
 
-        <div className="flex justify-center">
-          <SearchCard fetchData={fetchData}/>
+      {posts.length > 0 && (
+        <div className="result div">
+          <ResultComponent posts={posts} />
         </div>
-
-        <div className="result div"> 
-          <ResultComponent posts={posts}/>
-        </div>
+      )}
     </>
   );
 };
